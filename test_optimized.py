8e1906306974c648e9d737a031950c76e93a3771
gustavo.aragon <gustavo.alejandro.aragon.cabrera@fit.fraunhofer.de>
2018-02-08 09:55:45 +0100

    Changed test_optimized.py 4

--- test_optimized.py
+++ test_optimized.py
@@ -256,7 +256,7 @@ one_ph_load_model = df1['Active Power phase R']
 #######################################################################
 ##########Calculation with PV and storage
 #######################################################################
-os.chdir(r'C:\Users\guemruekcue\internship\optimization-agent')
+os.chdir(r'U:\Projekte\UCC\Storage4Grid\Simulation\python\FIT Repository\optimization-agent')
 script_dir = os.path.dirname(__file__)
 results_dir = os.path.join(os.path.dirname(__file__), 'results/')
 voltages=[]
