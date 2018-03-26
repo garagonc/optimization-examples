c8c8a310b10c53dc99b4a6fde53f65d16f050637
guemruekcue <erdem.guemruekcue@fit.fraunhofer.de>
2018-03-26 17:07:45 +0200

    Complete scenario implemented

--- stochastic/ElectricalVehicles/models/Script.py
+++ stochastic/ElectricalVehicles/models/Script.py
@@ -8,87 +8,17 @@ Created on Fri Mar 16 15:05:36 2018
 from pyomo.environ import SolverFactory
 from ReferenceModel import model
 import os
-import pandas as pd
 
 project_dir=os.path.dirname(os.path.dirname(__file__))
 
-data_file_LDLG=project_dir+'/scenariodata/LowDemLowGenScenario.dat'
-data_file_LDHG=project_dir+'/scenariodata/LowDemHighGenScenario.dat'
-data_file_HDLG=project_dir+'/scenariodata/HighDemLowGenScenario.dat'
-data_file_HDHG=project_dir+'/scenariodata/HighDemHighGenScenario.dat'
+data_file=project_dir+'/scenariodata/EarlyHighScenario.dat'
 
 
-
-instance1 = model.create_instance(data_file_LDLG)
-instance2= model.create_instance(data_file_LDHG)
-instance3= model.create_instance(data_file_HDLG)
-instance4= model.create_instance(data_file_HDHG)
-
+instance = model.create_instance(data_file)
+instance.pprint()
 
 opt=SolverFactory("ipopt")
-results1=opt.solve(instance1)
-results2=opt.solve(instance2)
-results3=opt.solve(instance3)
-results4=opt.solve(instance4)
+results=opt.solve(instance)
 
-
-def makelist(sortedlist):
-    x, y = zip(*sortedlist)
-    output=[]
-    for value in y:
-        output.append(value.value)
-    return output
-
-
-
-listsPV1=sorted(instance1.pPV.items())
-listsPV2=sorted(instance2.pPV.items())   
-listsPV3=sorted(instance3.pPV.items())
-listsPV4=sorted(instance4.pPV.items())       
-
-listsSOC1= sorted(instance1.SoC.items())
-listsSOC2= sorted(instance2.SoC.items()) 
-listsSOC3= sorted(instance3.SoC.items())
-listsSOC4= sorted(instance4.SoC.items())
-
-listsPBat1 = sorted(instance1.PBAT.items())
-listsPBat2 = sorted(instance2.PBAT.items())   
-listsPBat3 = sorted(instance3.PBAT.items())
-listsPBat4 = sorted(instance3.PBAT.items())
-
-listsPGrid1 = sorted(instance1.PGRID.items())
-listsPGrid2 = sorted(instance2.PGRID.items())   
-listsPGrid3 = sorted(instance3.PGRID.items())
-listsPGrid4 = sorted(instance4.PGRID.items())
-
-PV1=makelist(listsPV1)
-PV2=makelist(listsPV2)
-PV3=makelist(listsPV3)
-PV4=makelist(listsPV4)
-
-SOC1=makelist(listsSOC1)
-SOC2=makelist(listsSOC2)   
-SOC3=makelist(listsSOC3) 
-SOC4=makelist(listsSOC4) 
-
-PBAT1=makelist(listsPBat1)
-PBAT2=makelist(listsPBat2)   
-PBAT3=makelist(listsPBat3)
-PBAT4=makelist(listsPBat4)
-
-PGRID1=makelist(listsPGrid1)
-PGRID2=makelist(listsPGrid2)
-PGRID3=makelist(listsPGrid3)
-PGRID4=makelist(listsPGrid4)
-
-df_SOC=pd.DataFrame(list(zip(SOC1, SOC2,SOC3,SOC4)),columns=['Scenario1','Scenario2','Scenario3','Scenario4'])
-df_pPV=pd.DataFrame(list(zip(PV1, PV2,PV3,PV4)),columns=['Scenario1','Scenario2','Scenario3','Scenario4'])
-df_PBAT=pd.DataFrame(list(zip(PBAT1, PBAT2,PBAT3,PBAT4)),columns=['Scenario1','Scenario2','Scenario3','Scenario4'])
-df_PGRID=pd.DataFrame(list(zip(PGRID1, PGRID2,PGRID3,PGRID4)),columns=['Scenario1','Scenario2','Scenario3','Scenario4'])
-
-print("Objective function values")
-print("1:",instance1.Obj())
-print("2:",instance2.Obj())
-print("3:",instance3.Obj())
-print("4",instance4.Obj())
-
+print(results)
+""""""
--- stochastic/ElectricalVehicles/scheduleGenerator/cdf.py
+++ stochastic/ElectricalVehicles/scheduleGenerator/cdf.py
@@ -36,6 +36,10 @@ print("Between 18:00 and 24:00:",pdf_leave.cdf(24*60)-pdf_leave.cdf(18*60))
 print()
 
 print("Last arrival probabilities")
+veryEarly=pdf_arrival.cdf(14*60)
+Early=pdf_arrival.cdf(18*60)-pdf_arrival.cdf(14*60)
+Normal=pdf_arrival.cdf(22*60)-pdf_arrival.cdf(18*60)
+Late=pdf_arrival.cdf(24*60)-pdf_arrival.cdf(22*60)
 print("Between 00:00 and 14:00:",pdf_arrival.cdf(14*60))
 print("Between 14:00 and 18:00:",pdf_arrival.cdf(18*60)-pdf_arrival.cdf(14*60))
 print("Between 18:00 and 22:00:",pdf_arrival.cdf(22*60)-pdf_arrival.cdf(18*60))
@@ -43,10 +47,39 @@ print("Between 22:00 and 24:00:",pdf_arrival.cdf(24*60)-pdf_arrival.cdf(22*60))
 print()
 
 print("DOD")
+HighSOC=pdf_eleCon.cdf(7.5)
+MedSOC=pdf_eleCon.cdf(15)-pdf_eleCon.cdf(7.5)
+LowSOC=pdf_eleCon.cdf(22.5)-pdf_eleCon.cdf(15)
+VLowSOC=pdf_eleCon.cdf(30)-pdf_eleCon.cdf(22.5)
+
 print("Between 00-25% :",pdf_eleCon.cdf(7.5))
 print("Between 25-50% :",pdf_eleCon.cdf(15)-pdf_eleCon.cdf(7.5))
 print("Between 50-75% :",pdf_eleCon.cdf(22.5)-pdf_eleCon.cdf(15))
 print("Between 75-100%:",pdf_eleCon.cdf(30)-pdf_eleCon.cdf(22.5))
 print()
 
+earlyLow=Early*LowSOC
+earlyMed=Early*MedSOC
+earlyHigh=Early*HighSOC
+normalLow=Normal*LowSOC
+normalMed=Normal*MedSOC
+normalHigh=Normal*HighSOC
+lateLow=Late*LowSOC
+lateMed=Late*MedSOC
+lateHigh=Late*HighSOC
 
+norm=1/(earlyLow+earlyMed+earlyHigh+normalLow+normalMed+normalHigh+lateLow+lateMed+lateHigh)
+
+normalized_earlyLow=earlyLow*norm
+normalized_earlyMed=earlyMed*norm
+normalized_earlyHigh=earlyHigh*norm
+normalized_normalLow=normalLow*norm
+normalized_normalMed=normalMed*norm
+normalized_normalHigh=normalHigh*norm
+normalized_lateLow=lateLow*norm
+normalized_lateMed=lateMed*norm
+normalized_lateHigh=lateHigh*norm
+
+normalized_sum=normalized_earlyLow+normalized_earlyMed+normalized_earlyHigh+normalized_normalLow+normalized_normalMed+normalized_normalHigh+normalized_lateLow+normalized_lateMed+normalized_lateHigh
+
+print(normalized_sum)
