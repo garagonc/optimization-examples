76b4c4b3881dc71f59e597fc416f518e4100af09
guemruekcue <erdem.guemruekcue@fit.fraunhofer.de>
2018-04-18 14:32:42 +0200

    Scripts for deterministic and stochastic solutions

--- StochasticPrograming/WithRealTimeInfo/Version1/Script_forDeterministic.py
+++ StochasticPrograming/WithRealTimeInfo/Version1/Script_forDeterministic.py
@@ -30,97 +30,88 @@ results1=opt.solve(scenario1)
 results2=opt.solve(scenario2)
 results3=opt.solve(scenario3)
 
-PV_potential_1=[scenario1.P_PV_Value]
-P_Load_1=[scenario1.P_Load_Value]
-EV_ParkAtHome_1=[1]
-EV_ParkAway_1=[0]
-P_PV_1=[scenario1.P_PV_Output_ini()]
-P_Grid_1=[scenario1.P_Grid_Output_ini()]
-P_EV_HomeCharge_1=[scenario1.P_EV_Charge_ini()]
-P_EV_AwayCharge_1=[0.0]
-P_EV_Discharge_1=[0.0]
-P_ESS_1=[scenario1.P_ESS_Output_ini()]
-SoC_ESS_1=[scenario1.ESS_SoC_Value()]
-SoC_EV_1=[scenario1.EV_SoC_Value()]
 
-PV_potential_2=[scenario2.P_PV_Value]
-P_Load_2=[scenario2.P_Load_Value]
-EV_ParkAtHome_2=[1]
-EV_ParkAway_2=[0]
-P_PV_2=[scenario2.P_PV_Output_ini()]
-P_Grid_2=[scenario2.P_Grid_Output_ini()]
-P_EV_HomeCharge_2=[scenario2.P_EV_Charge_ini()]
-P_EV_AwayCharge_2=[0.0]
-P_EV_Discharge_2=[0.0]
-P_ESS_2=[scenario2.P_ESS_Output_ini()]
-SoC_ESS_2=[scenario2.ESS_SoC_Value()]
-SoC_EV_2=[scenario2.EV_SoC_Value()]
+PV_potential_1=[]
+P_Load_1=[]
+EV_ParkAtHome_1=[]
+EV_ParkAway_1=[]
+P_PV_1=[]
+P_Grid_1=[]
+P_EV_HomeCharge_1=[]
+P_EV_AwayCharge_1=[]
+P_EV_Discharge_1=[]
+P_ESS_1=[]
+SoC_ESS_1=[]
+SoC_EV_1=[]
 
-PV_potential_3=[scenario3.P_PV_Value]
-P_Load_3=[scenario3.P_Load_Value]
-EV_ParkAtHome_3=[1]
-EV_ParkAway_3=[0]
-P_PV_3=[scenario1.P_PV_Output_ini()]
-P_Grid_3=[scenario1.P_Grid_Output_ini()]
-P_EV_HomeCharge_3=[scenario3.P_EV_Charge_ini()]
-P_EV_AwayCharge_3=[0.0]
-P_EV_Discharge_3=[0.0]
-P_ESS_3=[scenario3.P_ESS_Output_ini()]
-SoC_ESS_3=[scenario3.ESS_SoC_Value()]
-SoC_EV_3=[scenario3.EV_SoC_Value()]
+PV_potential_2=[]
+P_Load_2=[]
+EV_ParkAtHome_2=[]
+EV_ParkAway_2=[]
+P_PV_2=[]
+P_Grid_2=[]
+P_EV_HomeCharge_2=[]
+P_EV_AwayCharge_2=[]
+P_EV_Discharge_2=[]
+P_ESS_2=[]
+SoC_ESS_2=[]
+SoC_EV_2=[]
+
+
+PV_potential_3=[]
+P_Load_3=[]
+EV_ParkAtHome_3=[]
+EV_ParkAway_3=[]
+P_PV_3=[]
+P_Grid_3=[]
+P_EV_HomeCharge_3=[]
+P_EV_AwayCharge_3=[]
+P_EV_Discharge_3=[]
+P_ESS_3=[]
+SoC_ESS_3=[]
+SoC_EV_3=[]
 
 
 # %%
-for t in range(23):      
-    P_Load_1.append(scenario1.P_Load_Forecast[t+1])
-    P_Load_2.append(scenario2.P_Load_Forecast[t+1])
-    P_Load_3.append(scenario3.P_Load_Forecast[t+1])
-    
-    PV_potential_1.append(scenario1.P_PV_Forecast[t+1])
-    PV_potential_2.append(scenario2.P_PV_Forecast[t+1])
-    PV_potential_3.append(scenario3.P_PV_Forecast[t+1])
-    
-    EV_ParkAtHome_1.append(scenario1.EV_ParkAtHome_Forecast[t+1])
-    EV_ParkAtHome_2.append(scenario2.EV_ParkAtHome_Forecast[t+1])
-    EV_ParkAtHome_3.append(scenario3.EV_ParkAtHome_Forecast[t+1])
-    
-    EV_ParkAway_1.append(scenario1.EV_ParkAway_Forecast[t+1])
-    EV_ParkAway_2.append(scenario2.EV_ParkAway_Forecast[t+1])
-    EV_ParkAway_3.append(scenario3.EV_ParkAway_Forecast[t+1])
-    
-    P_EV_Discharge_1.append(scenario1.P_EV_DriveDemand[t+1])
-    P_EV_Discharge_2.append(scenario2.P_EV_DriveDemand[t+1])
-    P_EV_Discharge_3.append(scenario3.P_EV_DriveDemand[t+1])      
-    
-    P_PV_1.append(scenario1.P_PV_Output[t+1]())
-    P_PV_2.append(scenario2.P_PV_Output[t+1]())
-    P_PV_3.append(scenario3.P_PV_Output[t+1]())
-    
-    P_Grid_1.append(scenario1.P_Grid_Output[t+1]())
-    P_Grid_2.append(scenario2.P_Grid_Output[t+1]())
-    P_Grid_3.append(scenario3.P_Grid_Output[t+1]())
-    
-    P_ESS_1.append(scenario1.P_ESS_Output[t+1]())
-    P_ESS_2.append(scenario2.P_ESS_Output[t+1]())
-    P_ESS_3.append(scenario3.P_ESS_Output[t+1]())
-    
-    P_EV_HomeCharge_1.append(scenario1.P_EV_Charge_AtHome[t+1]())
-    P_EV_HomeCharge_2.append(scenario2.P_EV_Charge_AtHome[t+1]())
-    P_EV_HomeCharge_3.append(scenario3.P_EV_Charge_AtHome[t+1]())
-    
-    P_EV_AwayCharge_1.append(scenario1.P_EV_Charge_Away[t+1]())
-    P_EV_AwayCharge_2.append(scenario2.P_EV_Charge_Away[t+1]())
-    P_EV_AwayCharge_3.append(scenario3.P_EV_Charge_Away[t+1]())
-    
-    SoC_ESS_1.append(scenario1.SoC_ESS[t+1]())
-    SoC_ESS_2.append(scenario2.SoC_ESS[t+1]())
-    SoC_ESS_3.append(scenario3.SoC_ESS[t+1]())
-    
-    SoC_EV_1.append(scenario1.SoC_EV[t+1]())
-    SoC_EV_2.append(scenario2.SoC_EV[t+1]())
-    SoC_EV_3.append(scenario3.SoC_EV[t+1]())
+for t in range(24):      
+    P_Load_1.append(scenario1.P_Load_Forecast[t])
+    PV_potential_1.append(scenario1.P_PV_Forecast[t])
+    EV_ParkAtHome_1.append(scenario1.EV_ParkAtHome_Forecast[t])
+    EV_ParkAway_1.append(scenario1.EV_ParkAway_Forecast[t])
+    P_EV_Discharge_1.append(scenario1.P_EV_DriveDemand[t])      
+    P_PV_1.append(scenario1.P_PV_Output_ini[t]()                if t==0 else scenario1.P_PV_Output_rec[t]())
+    P_Grid_1.append(scenario1.P_Grid_Output_ini[t]()            if t==0 else scenario1.P_Grid_Output_rec[t]())
+    P_ESS_1.append(scenario1.P_ESS_Output_ini[t]()              if t==0 else scenario1.P_ESS_Output_rec[t]())
+    P_EV_HomeCharge_1.append(scenario1.P_EV_ChargeHome_ini[t]() if t==0 else scenario1.P_EV_ChargeHome_rec[t]())
+    P_EV_AwayCharge_1.append(scenario1.P_EV_ChargeAway_ini[t]() if t==0 else scenario1.P_EV_ChargeAway_rec[t]())
+    SoC_ESS_1.append(scenario1.SoC_ESS[t]())
+    SoC_EV_1.append(scenario1.SoC_EV[t]())
 
-    
+    P_Load_2.append(scenario2.P_Load_Forecast[t])
+    PV_potential_2.append(scenario2.P_PV_Forecast[t])
+    EV_ParkAtHome_2.append(scenario2.EV_ParkAtHome_Forecast[t])
+    EV_ParkAway_2.append(scenario2.EV_ParkAway_Forecast[t])
+    P_EV_Discharge_2.append(scenario2.P_EV_DriveDemand[t])      
+    P_PV_2.append(scenario2.P_PV_Output_ini[t]()                if t==0 else scenario2.P_PV_Output_rec[t]())
+    P_Grid_2.append(scenario2.P_Grid_Output_ini[t]()            if t==0 else scenario2.P_Grid_Output_rec[t]())
+    P_ESS_2.append(scenario2.P_ESS_Output_ini[t]()              if t==0 else scenario2.P_ESS_Output_rec[t]())
+    P_EV_HomeCharge_2.append(scenario2.P_EV_ChargeHome_ini[t]() if t==0 else scenario2.P_EV_ChargeHome_rec[t]())
+    P_EV_AwayCharge_2.append(scenario2.P_EV_ChargeAway_ini[t]() if t==0 else scenario2.P_EV_ChargeAway_rec[t]())
+    SoC_ESS_2.append(scenario2.SoC_ESS[t]())
+    SoC_EV_2.append(scenario2.SoC_EV[t]())
+
+    P_Load_3.append(scenario3.P_Load_Forecast[t])
+    PV_potential_3.append(scenario3.P_PV_Forecast[t])
+    EV_ParkAtHome_3.append(scenario3.EV_ParkAtHome_Forecast[t])
+    EV_ParkAway_3.append(scenario3.EV_ParkAway_Forecast[t])
+    P_EV_Discharge_3.append(scenario3.P_EV_DriveDemand[t])      
+    P_PV_3.append(scenario3.P_PV_Output_ini[t]()                if t==0 else scenario3.P_PV_Output_rec[t]())
+    P_Grid_3.append(scenario3.P_Grid_Output_ini[t]()            if t==0 else scenario3.P_Grid_Output_rec[t]())
+    P_ESS_3.append(scenario3.P_ESS_Output_ini[t]()              if t==0 else scenario3.P_ESS_Output_rec[t]())
+    P_EV_HomeCharge_3.append(scenario3.P_EV_ChargeHome_ini[t]() if t==0 else scenario3.P_EV_ChargeHome_rec[t]())
+    P_EV_AwayCharge_3.append(scenario3.P_EV_ChargeAway_ini[t]() if t==0 else scenario3.P_EV_ChargeAway_rec[t]())
+    SoC_ESS_3.append(scenario3.SoC_ESS[t]())
+    SoC_EV_3.append(scenario3.SoC_EV[t]())
 
 scenario1_Df=pandas.DataFrame(list(zip(EV_ParkAtHome_1,EV_ParkAway_1,P_EV_HomeCharge_1,P_EV_AwayCharge_1,P_EV_Discharge_1,SoC_EV_1)),
                         columns=['Homepark','Awaypark','EV Charge at Home','EV Charge Away','EV Discharge','EV SOC'])
--- StochasticPrograming/WithRealTimeInfo/Version1/Script_forStochastic.py
+++ StochasticPrograming/WithRealTimeInfo/Version1/Script_forStochastic.py
@@ -5,6 +5,5 @@ Created on Tue Apr 17 15:22:05 2018
 @author: guemruekcue
 """
 
-import os
 from subprocess import call
 call("runph --model-directory=models --instance-directory=scenariodata --default-rho=1 --solver=ipopt --solution-writer=pyomo.pysp.plugins.csvsolutionwriter")
