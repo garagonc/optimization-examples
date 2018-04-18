28bae357bee334e1ea19c26d578ad63522807291
guemruekcue <erdem.guemruekcue@fit.fraunhofer.de>
2018-04-18 15:54:20 +0200

    Importing the results to the script

--- StochasticPrograming/WithRealTimeInfo/Version0/Script_forStochastic.py
+++ StochasticPrograming/WithRealTimeInfo/Version0/Script_forStochastic.py
@@ -7,3 +7,79 @@ Created on Tue Apr 17 15:22:05 2018
 
 from subprocess import call
 call("runph --model-directory=models --instance-directory=scenariodata --default-rho=1 --solver=ipopt --solution-writer=pyomo.pysp.plugins.csvsolutionwriter")
+
+#%%
+import csv
+csv_file = "ph.csv"
+
+P_Grid_Output_ini  ={}
+P_PV_Output_ini    ={}
+P_ESS_Output_ini   ={}
+P_EV_ChargeAway_ini={}
+P_EV_ChargeHome_ini={}
+
+P_Grid_Output_lng  ={}
+P_PV_Output_lng    ={}
+P_ESS_Output_lng   ={}
+P_EV_ChargeAway_lng={}
+P_EV_ChargeHome_lng={}
+
+P_Grid_Output_med  ={}
+P_PV_Output_med    ={}
+P_ESS_Output_med   ={}
+P_EV_ChargeAway_med={}
+P_EV_ChargeHome_med={}
+
+P_Grid_Output_sho  ={}
+P_PV_Output_sho    ={}
+P_ESS_Output_sho   ={}
+P_EV_ChargeAway_sho={}
+P_EV_ChargeHome_sho={}
+
+with open(csv_file) as f:
+    for line in f:
+        row=line.split(",")
+        if row[1]==' RootNode':
+            if row[2]==' P_ESS_Output_ini':
+                P_ESS_Output_ini[int(row[3])]=float(row[4])
+            elif row[2]==' P_EV_ChargeAway_ini':
+                P_EV_ChargeAway_ini[int(row[3])]=float(row[4])
+            elif row[2]==' P_EV_ChargeHome_ini':
+                P_EV_ChargeHome_ini[int(row[3])]=float(row[4])            
+            elif row[2]==' P_Grid_Output_ini':
+                P_Grid_Output_ini[int(row[3])]=float(row[4])            
+            elif row[2]==' P_PV_Output_ini':
+                P_PV_Output_ini[int(row[3])]=float(row[4])
+        elif row[1]==' EarlyLeaveLongCommuteNode':
+            if row[2]==' P_ESS_Output_rec':
+                P_ESS_Output_lng[int(row[3])]=float(row[4])
+            elif row[2]==' P_EV_ChargeAway_rec':
+                P_EV_ChargeAway_lng[int(row[3])]=float(row[4])
+            elif row[2]==' P_EV_ChargeHome_rec':
+                P_EV_ChargeHome_lng[int(row[3])]=float(row[4])            
+            elif row[2]==' P_Grid_Output_rec':
+                P_Grid_Output_lng[int(row[3])]=float(row[4])            
+            elif row[2]==' P_PV_Output_rec':
+                P_PV_Output_lng[int(row[3])]=float(row[4])
+        elif row[1]==' EarlyLeaveMedCommuteNode':
+            if row[2]==' P_ESS_Output_rec':
+                P_ESS_Output_med[int(row[3])]=float(row[4])
+            elif row[2]==' P_EV_ChargeAway_rec':
+                P_EV_ChargeAway_med[int(row[3])]=float(row[4])
+            elif row[2]==' P_EV_ChargeHome_rec':
+                P_EV_ChargeHome_med[int(row[3])]=float(row[4])            
+            elif row[2]==' P_Grid_Output_rec':
+                P_Grid_Output_med[int(row[3])]=float(row[4])            
+            elif row[2]==' P_PV_Output_rec':
+                P_PV_Output_med[int(row[3])]=float(row[4])
+        elif row[1]==' EarlyLeaveShortCommuteNode':
+            if row[2]==' P_ESS_Output_rec':
+                P_ESS_Output_sho[int(row[3])]=float(row[4])
+            elif row[2]==' P_EV_ChargeAway_rec':
+                P_EV_ChargeAway_sho[int(row[3])]=float(row[4])
+            elif row[2]==' P_EV_ChargeHome_rec':
+                P_EV_ChargeHome_sho[int(row[3])]=float(row[4])            
+            elif row[2]==' P_Grid_Output_rec':
+                P_Grid_Output_sho[int(row[3])]=float(row[4])            
+            elif row[2]==' P_PV_Output_rec':
+                P_PV_Output_sho[int(row[3])]=float(row[4])        
