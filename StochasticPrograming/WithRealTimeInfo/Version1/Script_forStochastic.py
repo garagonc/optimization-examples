6a92833a9e0de41ae015483a2237920fb9e34b39
guemruekcue <erdem.guemruekcue@fit.fraunhofer.de>
2018-04-18 14:44:11 +0200

    Removed

--- StochasticPrograming/WithRealTimeInfo/Version1/Script_forStochastic.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 17 15:22:05 2018
-
-@author: guemruekcue
-"""
-
-from subprocess import call
-call("runph --model-directory=models --instance-directory=scenariodata --default-rho=1 --solver=ipopt --solution-writer=pyomo.pysp.plugins.csvsolutionwriter")
