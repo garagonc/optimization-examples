b9f162f368bc0ff87ca25ec47c9257b2e0797b76
guemruekcue <erdem.guemruekcue@fit.fraunhofer.de>
2018-05-04 15:21:16 +0200

    Package for stochastic dynamic programming

---
+++ Optimization Models/stochastic_optimization/dynamic_programming/script_forDynamicProg.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 25 16:48:49 2018
+
+@author: guemruekcue
+"""
+
+#Minimum grid exchange with 0.1 SOC discretization
+from ScenarioClass import scenario_24Ts
+from DynamicProgram import StochasticDynamicProgram
+from pyomo.environ import SolverFactory
+import time
+import os
+
+# %%
+pbmatrix='MarkovModel.csv'
+pbmatrix2='EVSoCModel.csv'
+
+prob_plug={}
+with open(pbmatrix) as f:
+    for line in f:
+        row=line.split(",")
+        prob_plug[int(row[0]),int(row[1]),int(row[2])]=float(row[3])        
+
+prob_end_soc={}
+with open(pbmatrix2) as f2:
+    for line in f2:
+        row=line.split(",")
+        prob_end_soc[int(row[0])]=float(row[1])   
+
+
+# %%
+opt1=SolverFactory('glpk',executable="C:/Users/guemruekcue/Anaconda3/pkgs/glpk-4.63-vc14_0/Library/bin/glpsol")
+opt2= SolverFactory("ipopt", executable="C:/Users/guemruekcue/Anaconda3/pkgs/ipopt-3.11.1-2/Library/bin/ipopt")
+opt3= SolverFactory("bonmin", executable="C:/cygwin/home/bonmin/Bonmin-1.8.6/build/bin/bonmin")
+
+ess_domain=[20,30,40,50,60,70,80,90,100]
+ev_domain=[30,40,50,60,70]
+
+
+dynprog=StochasticDynamicProgram(scenario_24Ts,ess_domain,ev_domain,opt1,1,prob_plug,prob_end_soc)
+
+
+start_time=time.time()
+dynprog.wholeMapCalculation()
+end_time=time.time()
+print("Execution completed in",end_time-start_time,"seconds")
+      
+# %%  
+P_Grid_Output  =[]
+P_PV_Output    =[]
+P_ESS_Output   =[]
+P_EV_ChargeHome=[]
+
+Position=dict.fromkeys(range(24),1)
+for ts in range(7,21):
+    Position[ts]=0
+
+EV_expected=dict.fromkeys(range(7,21),50)
+
+for ts in range(24):
+    if ts==0:
+        essSoc=40
+        ev_Soc_calc=30
+        
+    evSoc=ev_Soc_calc if Position[ts]==1 else EV_expected[ts]
+        
+    print(ts,essSoc,evSoc,Position[ts])    
+    results=dynprog.findoptimalValues(ts,essSoc,evSoc,Position[ts])   
+    P_PV_Output.append(results[0])
+    P_ESS_Output.append(results[1])
+    P_Grid_Output.append(results[2])
+    P_EV_ChargeHome.append(results[3])
+    
+    ess_Soc=results[4]
+    ev_Soc_calc=results[4]
+    
+print(results)
+""""""
--- Optimization Models/stochastic_optimization/stochastic_programming/Script_forStochastic.py
+++ Optimization Models/stochastic_optimization/stochastic_programming/Script_forStochastic.py
@@ -55,7 +55,7 @@ def solve_stochastic_programming(timestep,SoC_ESS,SoC_EV,Position_EV=True):
     return new_SoC_ESS,new_SoC_EV
 
 #%%
-ev_min=0.45    
+ev_min=0.50    
 
 ess0,ev0=0.40,0.30
 ess1 ,ev1 =solve_stochastic_programming(0 ,ess0 ,ev0)
@@ -65,24 +65,19 @@ ess4 ,ev4 =solve_stochastic_programming(3 ,ess3 ,ev3)
 ess5 ,ev5 =solve_stochastic_programming(4 ,ess4 ,ev4)
 ess6 ,ev6 =solve_stochastic_programming(5 ,ess5 ,ev5)
 ess7 ,ev7 =solve_stochastic_programming(6 ,ess6 ,ev6)
-ess8 ,ev8 =solve_stochastic_programming(7 ,ess7 ,ev7)
-ess9 ,ev9 =solve_stochastic_programming(8 ,ess8 ,ev8)
-ess10,ev10=solve_stochastic_programming(9 ,ess9 ,ev9)
-ess11,ev11=solve_stochastic_programming(10,ess10,ev10)
-ess12,ev12=solve_stochastic_programming(11,ess11,ev11)
-ess13,ev13=solve_stochastic_programming(12,ess12,ev12)
-ess14,ev14=solve_stochastic_programming(13,ess13,ev13)
-ess15,ev15=solve_stochastic_programming(14,ess14,ev14)
-ess16,ev16=solve_stochastic_programming(15,ess15,ev15)
-ev16=ev_min
+ess8 ,ev8 =solve_stochastic_programming(7 ,ess7 ,ev7,False)
+ess9 ,ev9 =solve_stochastic_programming(8 ,ess8 ,ev8,False)
+ess10,ev10=solve_stochastic_programming(9 ,ess9 ,ev9,False)
+ess11,ev11=solve_stochastic_programming(10,ess10,ev10,False)
+ess12,ev12=solve_stochastic_programming(11,ess11,ev11,False)
+ess13,ev13=solve_stochastic_programming(12,ess12,ev12,False)
+ess14,ev14=solve_stochastic_programming(13,ess13,ev13,False)
+ess15,ev15=solve_stochastic_programming(14,ess14,ev14,False)
+ess16,ev16=solve_stochastic_programming(15,ess15,ev15,False)
 ess17,ev17=solve_stochastic_programming(16,ess16,ev16,False)
-ev17=ev_min
 ess18,ev18=solve_stochastic_programming(17,ess17,ev17,False)
-ev18=ev_min
 ess19,ev19=solve_stochastic_programming(18,ess18,ev18,False)
-ev19=ev_min
 ess20,ev20=solve_stochastic_programming(19,ess19,ev19,False)
-ev20=ev_min
 ess21,ev21=solve_stochastic_programming(20,ess20,ev20,False)
 
 #%%
